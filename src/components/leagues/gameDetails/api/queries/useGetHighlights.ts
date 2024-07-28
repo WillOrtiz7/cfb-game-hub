4fe876980e6d3341c61eab4a8694d55c4ad87a85
willortiz7 <willortiz407@gmail.com>
2024-07-28 17:49:59 -0500

    Add get highlights query

---
+++ src/components/leagues/gameDetails/api/queries/useGetHighlights.ts
@@ -0,0 +1,36 @@
+import { supabase } from "@/supabase/createClient";
+import { useQuery } from "@tanstack/react-query";
+
+
+interface GetHighlightsResponse {
+    id: string;
+    schedule_id: string;
+    title?: string;
+    url: string;
+}
+
+async function getHighlights(scheduleId: string): Promise<GetHighlightsResponse[]> {
+  const { data, error } = await supabase
+    .from("highlights")
+    .select(`
+      id,
+      schedule_id,
+      title,
+      url
+    `).eq("schedule_id", scheduleId)
+
+  if (error) {
+    throw new Error("Error code: " + error.code + "\nFailed to fetch teams");
+  }
+
+    return data as GetHighlightsResponse[];
+}
+
+export function useGetHighlights(scheduleId: string) {
+  return useQuery({
+    queryKey: ["getHighlights"],
+    queryFn: async () => {
+      return getHighlights(scheduleId);
+    },
+  });
+}
--- src/components/leagues/gameDetails/components/GameDetailsHighlights.tsx
+++ src/components/leagues/gameDetails/components/GameDetailsHighlights.tsx
@@ -1,29 +1,45 @@
 import { Button } from "@/components/ui/button";
 import { useParams } from "@tanstack/react-router";
+import { useGetHighlights } from "../api/queries/useGetHighlights";
 import { GameDetailsUpsertHighlightsModal } from "./GameDetailsUpsertHighlightsModal";
 
 export default function GameDetailsHighlights() {
   const { scheduleId } = useParams({
     from: "/leagues/$leagueSlug/gameDetails/$scheduleId",
   });
+
+  const {
+    data: highlights,
+    isLoading,
+    isError,
+    error,
+  } = useGetHighlights(scheduleId);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div className="w-full">
-        <iframe
-          className="w-full h-80"
-          src="https://clips.twitch.tv/embed?clip=RichPerfectRaisinHassaanChop-KWduEdSWQygsIfGz&parent=cfbgamehub.netlify.app&parent=www.cfbgamehub.netlify.app"
-          title="Video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        ></iframe>
-      </div>
-      <div className="w-full">
-        <iframe
-          className="w-full h-80"
-          src="https://www.youtube.com/embed/gCCErCKOjjA?si=b33P83yHeUaMDBYN"
-          title="Video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        ></iframe>
-      </div>
+      <h3 className="text-2xl font-semibold">Highlights</h3>
+      {highlights && highlights.length > 0 ? (
+        highlights.map((highlight) => (
+          <div className="w-full" key={highlight.id}>
+            <iframe
+              className="w-full h-80"
+              src={highlight.url}
+              title="Video player"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            ></iframe>
+          </div>
+        ))
+      ) : (
+        <p>No highlights</p>
+      )}
+
       <GameDetailsUpsertHighlightsModal
         description="Add an iframe URL from YouTube or Twich"
         requestType="POST"
