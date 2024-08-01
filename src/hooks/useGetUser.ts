92cc31278e0996281f1b876f8995118861983df0
willortiz7 <willortiz407@gmail.com>
2024-07-31 21:47:00 -0500

    Check JWT

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/supabase/createClient";
 import { useUserStore } from "@/zustand/useUserStore";
+import { jwtDecode } from 'jwt-decode';
 import { useEffect } from "react";
 
 
@@ -10,6 +11,12 @@ export function useGetUser() {
     useEffect(() => {
       async function getUser() {
         const response  = await supabase.auth.getUser();
+        const session = await supabase.auth.getSession();
+        if (session.data.session){
+            console.log(jwtDecode(session.data.session.access_token));
+        } else {
+            console.log("no session");
+        }
         
         if (response.data.user) {
           setUser(response.data.user);
